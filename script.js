ce51a4d55cce49b9a8496f301b0bdf3b31f790a5
Nazanin Ashrafi <68081983+NazaNEYn@users.noreply.github.com>
2024-06-17 13:53:31 +0330

    Refactored some codes

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 // Generate a random number between 1 and 100 (inclusive)
-// we used let because we need to reassign the variable on the reset button
+// we used let because we need to reassign
+// the variable on the reset button
 let randomNumber = Math.trunc(Math.random() * 100 + 1);
+console.log(randomNumber);
 
 // Set the maximum number of allowed guesses
 const maxTries = 5;
@@ -10,8 +12,9 @@ const maxTries = 5;
 // Initialize a variable to keep track of attempts made
 let tries = 0;
 
-// For debugging purposes, you can uncomment this line to see the generated number
-console.log(randomNumber);
+const displayMessage = function (message) {
+  document.querySelector(".text").textContent = message;
+};
 
 // Add an event listener to the element with class "submit"
 document.querySelector(".submit").addEventListener("click", function () {
@@ -20,14 +23,14 @@ document.querySelector(".submit").addEventListener("click", function () {
 
   // Check if the input is empty or invalid (not a number or greater than 100)
   if (!pickedNumber || pickedNumber > 100) {
-    document.querySelector(".text").textContent = "Invalid number!";
+    displayMessage("Invalid number!");
   } else {
     // Increment the attempt counter
     tries++;
 
     // Check if the user guessed correctly
     if (pickedNumber === randomNumber) {
-      document.querySelector(".text").textContent = "You got it right!";
+      displayMessage("You got it right!");
 
       document.querySelector(".container").style.background = "#1c5d3b";
     } else {
@@ -38,20 +41,18 @@ document.querySelector(".submit").addEventListener("click", function () {
       if (remainingTries > 0) {
         // Provide feedback based on whether the guess was too high or too low
         if (pickedNumber > randomNumber) {
-          document.querySelector(
-            ".text"
-          ).textContent = `Too high! You've got ${remainingTries} more tries left.`;
+          displayMessage(
+            `Too high! You've got ${remainingTries} more tries left.`
+          );
         } else if (pickedNumber < randomNumber) {
-          document.querySelector(
-            ".text"
-          ).textContent = `Too low! You've got ${remainingTries} more tries left.`;
+          displayMessage(
+            `Too low! You've got ${remainingTries} more tries left.`
+          );
         }
       } else {
         // Display a message if the user loses (no tries left)
         // Reveal the correct answer
-        document.querySelector(
-          ".text"
-        ).textContent = `You lost! The correct answer is ${randomNumber}.`;
+        displayMessage(`You lost! The correct answer is ${randomNumber}.`);
         document.querySelector(".container").style.background = "#880212";
         // document.querySelector(".text").style.color = "#23252f";
       }
@@ -59,20 +60,16 @@ document.querySelector(".submit").addEventListener("click", function () {
   }
 });
 
-
 document.querySelector(".reset").addEventListener("click", function () {
   // Reload the whole page
   // window.location.reload();
 
   document.querySelector(".container").style.background = "#23252f";
-  document.querySelector(".text").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
   document.querySelector(".msg").value = "";
 
-  // setting the number of tries back to 0
   tries = 0;
 
   // we reassigned the variable
   randomNumber = Math.trunc(Math.random() * 100 + 1);
 });
-
-
